68636aa27d081ba2154568c976717f6f0dfaee8b
Wise Kwasi DUHO <WiseK@finonex.com>
2022-12-23 11:40:54 +0000

    completed post and comments repository

--- SM-Post/Post.Query/Post.Query.Infrastructure/Repositories/PostRepositories.cs
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Repositories/PostRepositories.cs
@@ -1,4 +1,5 @@
-﻿using Post.Query.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
 using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.DataAccess;
 
@@ -23,35 +24,61 @@ public class PostRepositories : IPostRepository
     public async Task UpdateAsync(PostEntity post)
     {
         await using var context = _factory.CreateDbContext();
+        context.Posts.Update(post);
+        _= await context.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(Guid postId)
     {
         await using var context = _factory.CreateDbContext();
+        var post = await GetByIdAsync(postId);
+        if (post==null)return;
+        context.Remove(post);
+        _ = await context.SaveChangesAsync();
+
     }
 
     public async Task<PostEntity> GetByIdAsync(Guid postId)
     {
         await using var context = _factory.CreateDbContext();
+        return await context.Posts
+            .Include(x=>x.Comments)
+            .FirstOrDefaultAsync(x=>x.PostId==postId);
     }
 
     public async Task<List<PostEntity>> ListAllAsync()
     {
         await using var context = _factory.CreateDbContext();
+        return await context.Posts.AsNoTracking()
+            .Include(x => x.Comments)
+            .AsNoTracking()
+            .ToListAsync();
     }
 
     public async Task<List<PostEntity>> ListByAuthorAsync(string author)
     {
         await using var context = _factory.CreateDbContext();
+        return await context.Posts.AsNoTracking().Include(x => x.Comments)
+            .AsNoTracking()
+            .Where(x=>x.Author.Contains(author))
+            .ToListAsync();
+
     }
 
-    public Task<List<PostEntity>> ListWithLikesAsync(int numberOfLikes)
+    public async Task<List<PostEntity>> ListWithLikesAsync(int numberOfLikes)
     {
         await using var context = _factory.CreateDbContext();
+        return await context.Posts.AsNoTracking().Include(x => x.Comments)
+            .AsNoTracking()
+            .Where(x => x.Likes>=numberOfLikes ).ToListAsync();
+
     }
 
     public async Task<List<PostEntity>> ListWIthCommentsAsync()
     {
         await using var context = _factory.CreateDbContext();
+        return await context.Posts.AsNoTracking().Include(x => x.Comments)
+            .AsNoTracking()
+            .Where(x => x.Comments != null && x.Comments.Any()).ToListAsync();
     }
 }
