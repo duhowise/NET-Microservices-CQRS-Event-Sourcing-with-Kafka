04d8c4798ac92ad0f50870e32f5ed92843475d01
wiseduho <wise.duho@leatherback.co>
2023-08-12 14:23:59 +0000

    Refactor CommandHandler to use IServiceScopeFactory
    Updated the CommandHandler class to make use of IServiceScopeFactory rather than direct dependency on IEventSourcingHandler. This change allows for better scope management, specifically in scenarios where having a scoped instance of IEventSourcingHandler is crucial. The change is aimed at improving robustness and maintainability of the codebase.

--- SM-Post/Post.Cmd/Post.Cmd.Api/Commands/CommandHandler.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/CommandHandler.cs
@@ -6,64 +6,78 @@ namespace Post.Cmd.Api.Commands;
 
 public class CommandHandler : ICommandHandler
 {
-    private readonly IEventSourcingHandler<PostAggregate> _eventSourcingHandler;
+    private readonly IServiceScopeFactory _scopeFactory;
 
-    public CommandHandler(IEventSourcingHandler<PostAggregate> eventSourcingHandler)
+    public CommandHandler(IServiceScopeFactory scopeFactory)
     {
-        _eventSourcingHandler = eventSourcingHandler;
+        _scopeFactory = scopeFactory;
     }
     public async ValueTask<Unit> Handle(NewPostCommand command, CancellationToken cancellationToken)
     {
+        using var serviceScope = _scopeFactory.CreateScope();
+        var eventSourcingHandler = serviceScope.ServiceProvider.GetService<IEventSourcingHandler<PostAggregate>>();
         var aggregate=new PostAggregate(command.Id,command.Author,command.Message);
-        await _eventSourcingHandler.SaveAsync(aggregate);
+        await eventSourcingHandler.SaveAsync(aggregate);
         return Unit.Value;
     }
 
     public async ValueTask<Unit> Handle(EditMessageCommand command, CancellationToken cancellationToken)
     {
-        var aggregate=  await _eventSourcingHandler.GetByIdAsync(command.Id);
+        using var serviceScope = _scopeFactory.CreateScope();
+        var eventSourcingHandler = serviceScope.ServiceProvider.GetService<IEventSourcingHandler<PostAggregate>>();
+        var aggregate=  await eventSourcingHandler.GetByIdAsync(command.Id);
         aggregate.EditMessage(command.Message);
-        await _eventSourcingHandler.SaveAsync(aggregate);
+        await eventSourcingHandler.SaveAsync(aggregate);
         return Unit.Value;
     }
 
     public async ValueTask<Unit> Handle(LikePostCommand command, CancellationToken cancellationToken)
     {
-        var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+        using var serviceScope = _scopeFactory.CreateScope();
+        var eventSourcingHandler = serviceScope.ServiceProvider.GetService<IEventSourcingHandler<PostAggregate>>();
+        var aggregate = await eventSourcingHandler.GetByIdAsync(command.Id);
         aggregate.LikePost();
-        await _eventSourcingHandler.SaveAsync(aggregate);
+        await eventSourcingHandler.SaveAsync(aggregate);
         return Unit.Value;
     }
 
     public async ValueTask<Unit> Handle(AddCommentCommand command, CancellationToken cancellationToken)
     {
-        var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+        using var serviceScope = _scopeFactory.CreateScope();
+        var eventSourcingHandler = serviceScope.ServiceProvider.GetService<IEventSourcingHandler<PostAggregate>>();
+        var aggregate = await eventSourcingHandler.GetByIdAsync(command.Id);
         aggregate.AddComment(command.Comment,command.UserName);
-        await _eventSourcingHandler.SaveAsync(aggregate);
+        await eventSourcingHandler.SaveAsync(aggregate);
         return Unit.Value;
     }
 
     public async ValueTask<Unit> Handle(EditCommentCommand command, CancellationToken cancellationToken)
     {
-        var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+        using var serviceScope = _scopeFactory.CreateScope();
+        var eventSourcingHandler = serviceScope.ServiceProvider.GetService<IEventSourcingHandler<PostAggregate>>();
+        var aggregate = await eventSourcingHandler.GetByIdAsync(command.Id);
         aggregate.EditComment(command.CommentId,command.Comment,command.UserName);
-        await _eventSourcingHandler.SaveAsync(aggregate);
+        await eventSourcingHandler.SaveAsync(aggregate);
         return Unit.Value;
     }
 
     public async ValueTask<Unit> Handle(RemoveCommentCommand command, CancellationToken cancellationToken)
     {
-        var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+        using var serviceScope = _scopeFactory.CreateScope();
+        var eventSourcingHandler = serviceScope.ServiceProvider.GetService<IEventSourcingHandler<PostAggregate>>();
+        var aggregate = await eventSourcingHandler.GetByIdAsync(command.Id);
         aggregate.RemoveComment(command.CommentId,command.UserName);
-        await _eventSourcingHandler.SaveAsync(aggregate);
+        await eventSourcingHandler.SaveAsync(aggregate);
         return Unit.Value;
     }
 
     public async ValueTask<Unit> Handle(DeletePostCommand command, CancellationToken cancellationToken)
     {
-        var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+        using var serviceScope = _scopeFactory.CreateScope();
+        var eventSourcingHandler = serviceScope.ServiceProvider.GetService<IEventSourcingHandler<PostAggregate>>();
+        var aggregate = await eventSourcingHandler.GetByIdAsync(command.Id);
         aggregate.DeletePost(command.UserName);
-        await _eventSourcingHandler.SaveAsync(aggregate);
+        await eventSourcingHandler.SaveAsync(aggregate);
         return Unit.Value;
     }
 }
